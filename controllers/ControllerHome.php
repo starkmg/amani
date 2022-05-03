71fefac37081b0c64046f9cc1e203eb14dcca586
Daniel TSHISEKEDI <daniel.tshisekedi@orange.com>
2022-05-03 07:30:03 +0100

    Login Page

--- controllers/ControllerHome.php
+++ controllers/ControllerHome.php
@@ -2,15 +2,15 @@
 class ControllerHome{
     public function __construct($url){
         global $message, $alert;
-        $dan = new ModelTraitement();
-        if(isset($_POST["uploadFile"])){
-            $retour = $dan->chargement($_POST);
-            if($retour) extract($retour);
-        }
-        if(isset($_POST["deleteChargement"])){
-            $retour = $dan->chargementDeletion($_POST);
-            if($retour) extract($retour);
-        }
+//        $dan = new ModelTraitement();
+//        if(isset($_POST["uploadFile"])){
+//            $retour = $dan->chargement($_POST);
+//            if($retour) extract($retour);
+//        }
+//        if(isset($_POST["deleteChargement"])){
+//            $retour = $dan->chargementDeletion($_POST);
+//            if($retour) extract($retour);
+//        }
         $this->display();
     }
     public function display(){
